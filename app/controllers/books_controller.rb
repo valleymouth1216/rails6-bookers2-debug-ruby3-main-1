461ae37dedd2d4340b770dc68f2616d3f693f9df
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-03-21 16:19:11 +0900

    20230321修正完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+
   end
 
   def index
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
