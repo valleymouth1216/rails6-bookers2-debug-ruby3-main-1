63f17a3588ac6f622454708055abc5d1a46f020a
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-03-27 23:17:56 +0900

    20230327_テスト4項目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update,:edit]
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:update,:edit]
+
 
   def show
     @user = User.find(params[:id])
