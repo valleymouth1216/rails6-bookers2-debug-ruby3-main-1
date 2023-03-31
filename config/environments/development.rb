acc3020fb618083f956dc8a2192670b939c9d32a
valleymouth1216 <okeitan3280@gmail.com>
2023-03-31 14:44:18 +0000

    20230331環境変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.hosts << "a8c9cb2b77b24968bf1f0967084f4a3f.vfs.cloud9.us-east-1.amazonaws.com"
-  config.hosts << "9a29b2a1d2b146abb92a1481706704cd.vfs.cloud9.us-east-1.amazonaws.com"
+  #config.hosts << "9a29b2a1d2b146abb92a1481706704cd.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "96847a1785254ff6968e2196c9f41638.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
